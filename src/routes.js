77a3ecbbacb9fdedcc3693c77e1d8ea48e09ae56
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-02-01 12:56:33 -0300

    configuring the first model with the route and connection to the database.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocket' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Reynaldo Layme',
+    email: 'onlyreynaldo@hotmail.com',
+    password_hash: '1234567'
+  });
+
+  res.json(user);
+});
 
 export default routes;
