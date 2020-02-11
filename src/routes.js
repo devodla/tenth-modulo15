8351829ead00cf96d6d4a7dc5a518be79825d4a6
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-02-10 21:40:32 -0300

    Added routes to list and schedule

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
+import AppointmentController from './app/controllers/AppointmentController';
 import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
