72e272cb9518636a84dd94fe1e2de357c202cdd9
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-02-12 22:44:07 -0300

    Our first email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '42632b8d9a258a',
+    pass: '5bd79a57eec601',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
