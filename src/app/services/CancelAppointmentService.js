59737da6809e9ec75b0aca2d5db73fec6f7daef1
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-04-12 21:32:29 -0300

    feat(appointment) abstracting the logic CancelSer

---
+++ src/app/services/CancelAppointmentService.js
@@ -0,0 +1,50 @@
+import { isBefore, subHours } from 'date-fns';
+
+import Queue from '../../lib/Queue';
+
+import CancellationMail from '../jobs/CancellationMail';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class CancelAppointmentService {
+  async run({ provider_id, userId }) {
+    const appointment = await Appointment.findByPk(provider_id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== userId) {
+      throw new Error("You don't have permission to cancel this appointment.");
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+    const dateAtual = new Date();
+
+    if (isBefore(dateWithSub, dateAtual)) {
+      throw new Error('You can only cancel appointment 2 hours in advance.');
+    }
+
+    appointment.canceled_at = dateAtual;
+
+    await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
+    });
+
+    return appointment;
+  }
+}
+
+export default new CancelAppointmentService();
